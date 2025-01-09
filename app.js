8c70f60a6d91997044d4666aff0fff04bf66e183
dzemil karisik <karisikdzemil@gmail.com>
2025-01-09 16:45:50 +0100

    header func, and some changes

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("radi")
+const headerUl = document.getElementById("header-ul");
+const logout = document.getElementById('logOut');
+const homeBtn = document.getElementById("home-btn");
+
+
+// const headerWorkHandler = (event) =>{
+//     if(event.target.tagName !== "UL"){
+//         location.pathname =
+//     }
+// }
+
+// headerUl.addEventListener("click", headerWorkHandler);
+logout.addEventListener("click", () => location.href = 'index.html');
+homeBtn.addEventListener("click", () => location.href="welcome.html#contactUs")
--- assets/Login.js
+++ assets/Login.js
@@ -4,78 +4,83 @@ const formBtn = document.getElementById("form-btn");
 const wrongEnterPass = document.getElementById("wrong-enter-pass");
 const wrongEnterEmail = document.getElementById("wrong-enter-email");
 
-
 const emailReg = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
+class Login {
+  constructor(email, password) {
+    this.email = email;
+    this.pass = password;
+    this.login = false;
+    this.check();
+  }
+  validateEmail() {
+    if (!emailReg.test(this.email)) {
+      wrongEnterEmail.textContent = "Enter a valid email address";
+      return false;
+    } else {
+      wrongEnterEmail.textContent = "";
+      return true;
+    }
+  }
+  validatePassword() {
+    wrongEnterPass.textContent = "";
+    if (!/^.{8,}$/.test(this.pass)) {
+      wrongEnterPass.textContent = "You must enter keyword with min 8 letter";
+      return false;
+    } else if (!this.pass.match(/\d+/)) {
+      wrongEnterPass.textContent = "You need to enter least one number";
+      return false;
+    } else if (!this.pass.match(/[A-Z]/)) {
+      wrongEnterPass.textContent = "You need to enter least one upper case";
+      return false;
+    } else {
+      wrongEnterPass.textContent = "";
+      return true;
+    }
+  }
+  check() {
+    const isEmailValid =  this.validateEmail();
+    const isPasswordValid = this.validatePassword();
+    this.login = isEmailValid && isPasswordValid;
+  }
 
-class Login{
-    constructor(email, password){
-        this.email = email;
-        this.pass = password;
-        this.login = false;
-        this.check()
-    }
-    validateEmail(){
-        if(!emailReg.test(this.email)){
-            wrongEnterEmail.textContent = 'Enter a valid email address';
-            return false;
-        }else{
-            wrongEnterEmail.textContent = '';
-            return true;
-        }
-    }
-    validatePassword(){
-        wrongEnterPass.textContent = '';
-        if(!/^.{8,}$/.test(this.pass)){
-            wrongEnterPass.textContent = "You must enter keyword with min 8 letter";
-            return false;
-        }else if(!this.pass.match(/\d+/)){
-            wrongEnterPass.textContent = "You need to enter least one number";
-            return false;
-        }else if(!this.pass.match(/[A-Z]/)){
-            wrongEnterPass.textContent = "You need to enter least one upper case";
-            return false;
-        }else{
-            wrongEnterPass.textContent = '';
-            return true;
-        }
-    }
-    check(){
-      this.validateEmail();
-      this.validatePassword();
-      this.loginSucces();
-    }
-
-    loginSucces(){
-        const isEmailValid = this.validateEmail();
-        const isPasswordValid = this.validatePassword();
-
-        this.login = isEmailValid && isPasswordValid;
-    }
 }
 
-class User{
-    constructor(email, password){
-        this.email = email;
-        this.password = password;
-    }
+class User {
+  constructor(email, password) {
+    this.email = email;
+    this.password = password;
+  }
+
+  matchProfile() {
+    const hash = CryptoJS.SHA256(this.password).toString()
+    fetch("./assets/users.JSON")
+      .then((response) => response.json())
+      .then(data => {
+        const loggedUser = data.find(user =>  user.email === this.email && user.password === hash);
+        if(loggedUser){
+            alert(`Email: ${loggedUser.email}`);
+            email.value = '';
+            password.value = '';
+            location.href = 'welcome.html';
+        }else{
+            wrongEnterPass.textContent = "Invalid email or password!";
+        }
+      }).catch(error => {
+        console.error(error);
+        wrongEnterPass.textContent = "An error occurred. Please try again later.";
+    });
+  }
 }
 
-
-formBtn.addEventListener("click", (event)=>{
-    event.preventDefault();
-    const tryLogin = new Login(email.value, password.value);
-    if(tryLogin.login){
-        const user = new User(tryLogin.email, tryLogin.pass);
-        const hash = CryptoJS.SHA256(user.password).toString();
-        console.log(hash);
-        email.value = '';
-        password.value = '';
-        console.log(user);
-        // location.href = '/Users/dzemilkarisik/Desktop/eRacun/index.html';
-    }
+formBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  const tryLogin = new Login(email.value, password.value);
+  if (tryLogin.login) {
+    const user = new User(tryLogin.email, tryLogin.pass);
+    user.matchProfile();
+  }
 });
-
 
 // Proba123
 // Dzemil2004
