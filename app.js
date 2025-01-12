453703d5d8049e0cad7def94d02979685ac80087
dzemil karisik <karisikdzemil@gmail.com>
2025-01-12 16:18:43 +0100

    onclick

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ submitFormModalBtn.addEventListener("click", () => {
 contactButton.addEventListener("click", validateContactHandler);
 
 removeModalButton.addEventListener("click", removeModalHandler);
-modalUl.addEventListener("touch", showModalHandler);
+// modalUl.addEventListener("", showModalHandler);
 logoutHeader.addEventListener("click", () => (location.href = "index.html"));
 logoutHamb.addEventListener("click", () => (location.href = "index.html"));
 hambUl.addEventListener("click", (event) => {
