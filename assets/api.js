5ee8a363666b8e540ff08f9cc908363ecd8ae6b5
dzemil karisik <karisikdzemil@gmail.com>
2025-01-19 15:33:51 +0100

    final changes

--- assets/api.js
+++ assets/api.js
@@ -1,29 +1,47 @@
 const consBtn = document.getElementById("cons-btn");
-const closeBillBtn = document.getElementById('close-bill');
+const closeBillBtn = document.getElementById("close-bill");
 const modal = document.querySelector(".bill-modal");
+const convertRsd = document.getElementById("convert-rsd");
 // const blackDrop = document.querySelector(".black-drop")
-async function process(){
-    try{
+async function process() {
+
+  try {
+    const standard = Number(document.getElementById("standardCons").value);
+    const cheap = Number(document.getElementById("cheapCons").value);
+    if(standard === 0 || cheap === 0){
+        alert("You need to enter some value!");
+        return
+    }else{
         modal.classList.toggle("visible");
         blackDrop.classList.toggle("visible");
-        const standard = Number(document.getElementById("standardCons").value);
-        const cheap = Number(document.getElementById("cheapCons").value);
-        const title = document.getElementById("bill-data-h3");
-        const response = await fetch(`https://vebdizajn-4.onrender.com/api/vebdizajn/racun-za-struju?skupaStruja=${cheap}&jeftinaStruja=${standard}`);
-        const data = await response.json();
-        console.log(data);
-        console.log(cheap, standard);
-        title.innerHTML = `<h4>Standard consuption for this period: <b>${standard}KW</b> </h4> <h4> Cheap consuption for this period: <b>${cheap}KW</b></h4><h4> Bill:  <b>${data.racun}EUR</h4>`
-        document.getElementById("standardCons").value = '';
-        document.getElementById("cheapCons").value = '';
-
-    }catch{
-        throw new Error("Something went wrong!!!");
-    }
+    const title = document.getElementById("bill-data-h3");
+    const response = await fetch(
+      `https://vebdizajn-4.onrender.com/api/vebdizajn/racun-za-struju?skupaStruja=${cheap}&jeftinaStruja=${standard}`
+    );
+    const data = await response.json();
+    console.log(data);
+    console.log(cheap, standard);
+    title.innerHTML = `<h4>Standard consuption for this period: <b>${standard}KW</b> </h4> 
+                            <h4> Cheap consuption for this period: <b>${cheap}KW</b></h4>
+                                                    <h4 id="bill-convert"> Bill: <span id="rsd"> ${data.racun}</span>EUR</h4>
+                         `;
+                         convertRsd.addEventListener("click", ()=>{
+                            let price = document.getElementById("rsd").textContent;
+                            price = Number(price) * 117;
+                             const billConvert = document.getElementById("bill-convert");
+                             billConvert.innerHTML = ` Bill: <span id="rsd"> ${price}</span>RSD`
+                     }, {once: true});
+    document.getElementById("standardCons").value = "";
+    document.getElementById("cheapCons").value = "";
+}
+  } catch {
+    throw new Error("Something went wrong!!!");
+  }
 }
 
 consBtn.addEventListener("click", process);
 closeBillBtn.addEventListener("click", () => {
-    modal.classList.toggle("visible");
-    blackDrop.classList.toggle("visible");
-})
+  modal.classList.toggle("visible");
+  blackDrop.classList.toggle("visible");
+});
+
